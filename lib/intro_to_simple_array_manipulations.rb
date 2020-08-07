52b40c734bdbbbd48cab8eaa7ed2ec24c7575a25
lively-system-6174 <wkalada1@gmail.com>
2020-08-07 03:25:44 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,3 +1,23 @@
+def using_concat(array1, array2)
+  array1.concat(array2)
+end
+
+def (array, element)
+  array.insert( element )
+end
+
+def 
+  
+end
+
+def 
+  
+end
+
+def 
+  
+end
+
 def 
   
 end
